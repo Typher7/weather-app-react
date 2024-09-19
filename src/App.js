f2d562aa264913e70e03ef9fec143f3a9d78fa16
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-09-18 21:44:07 -0500

    library imports

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Navbar from './Navbar'
 
 // Import the images
-import clearImage from './assets/clear_background.jpeg'
+import clearImage from './assets/clearsky2.jpg'
 import cloudyImage from './assets/cloudy_background.jpeg'
-import rainyImage from './assets/rainy_background.jpeg'
+import rainyImage from './assets/rainy.jpg'
 import thunderstormImage from './assets/thunder.jpg'
 import snowyImage from './assets/snowy.jpg'
-import drizzleImage from './assets/trees.jpeg'
 import defaultImage from './assets/sunrise.jpg'
 
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
-  const [bgImage, setBgImage] = useState(defaultImage)
+  const [backgroundImage, setBackgroundImage] = useState(defaultImage)
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
@@ -29,29 +28,27 @@ function App() {
   }
 
   useEffect(() => {
-    if (data.weather && data.weather[0]){
+    if (data.weather && data.weather[0]) {
       const weatherMain = data.weather[0].main.toLowerCase()
-      switch(weatherMain){
+      switch (weatherMain) {
         case 'clear':
-          setBgImage(clearImage)
+          setBackgroundImage(clearImage)
           break
         case 'clouds':
-          setBgImage(cloudyImage)
+          setBackgroundImage(cloudyImage)
           break
         case 'rain':
-          setBgImage(rainyImage)
-          break
         case 'drizzle':
-          setBgImage(drizzleImage)
+          setBackgroundImage(rainyImage)
           break
         case 'thunderstorm':
-          setBgImage(thunderstormImage)
+          setBackgroundImage(thunderstormImage)
           break
         case 'snow':
-          setBgImage(snowyImage)
+          setBackgroundImage(snowyImage)
           break
         default:
-          setBgImage(defaultImage)
+          setBackgroundImage(defaultImage)
       }
     }
   }, [data])
@@ -59,49 +56,46 @@ function App() {
   return (  
     <div>
       <Navbar />
-    <div className="app" style={{backgroundImage: bgImage}}>
-      <div className="search">
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type="text" />
-      </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
+      <div className="app" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="search">
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
         </div>
-
-        {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-              <p>Feels Like</p>
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
             </div>
-            <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
             </div>
-            <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Wind Speed</p>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
             </div>
           </div>
-        }
 
-
-
+          {data.name !== undefined &&
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          }
+        </div>
       </div>
-    </div>
     </div>
   );
 }
